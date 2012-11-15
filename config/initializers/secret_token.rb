496d40a822d3bf1399ddf08e19f931eecb94d551
Guillermo Iguaran <guilleiguaran@gmail.com>
2012-11-15 18:32:29 -0500

    Initial bootstrap for application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ScanToCall::Application.config.secret_token = 'eae9be80b04b84811241b2e0fe4731f2928be86549963ca6eca50d4c2f3a71f0b602797163aeac043d19a1ad66656f0aba421e46bc63f3cc4296b858df07e24e'
